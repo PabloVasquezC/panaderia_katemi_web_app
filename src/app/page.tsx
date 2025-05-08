608437303645d50c660f4b468713ea68924b9921
Pablo Vasquez <vascor.pablo@gmail.com>
2025-05-08 11:21:19 -0400

    setting katemi logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans bg-[#fff8f0] text-[#3b2f2f]">
       <main className="flex flex-col gap-8 row-start-2 items-center text-center max-w-2xl">
         <Image
-          src="/vercel.svg" // Asegúrate de tener un logo en public/logo_katemi.svg
+          src="/katemi_logo.png" 
           alt="Panadería Katemi"
           width={180}
           height={180}
